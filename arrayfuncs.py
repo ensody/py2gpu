825d4477f5441fc7ecac1d810a07e9b2ea27c918
Waldemar Kornewald <wkornewald@localhost>
2010-02-22 16:37:46 +0100

    added more array types

--- arrayfuncs.py
+++ arrayfuncs.py
@@ -1,8 +1,8 @@
-from py2gpu.api import blockwise, IntArray, FloatArray
+from py2gpu.api import blockwise, Int32Array, FloatArray
 from numpy import float32
 
 @blockwise({'data': ('height', 'width')},
-           {'data': IntArray, ('sum', 'return', 'height', 'width'): int})
+           {'data': Int32Array, ('sum', 'return', 'height', 'width'): int})
 def __array_sum2d(data, height, width):
     sum = 0
     for row in range(height):
@@ -15,7 +15,7 @@ blockwise({'data': ('height', 'width')},
           name='__array_fsum2d')(__array_sum2d)
 
 @blockwise({'data': ('height', 'width')},
-           {'data': IntArray, ('height', 'width'): int, 'return': float32})
+           {'data': Int32Array, ('height', 'width'): int, 'return': float32})
 def __array_mean2d(data, height, width):
     return float(data.sum()) / (height * width)
 
@@ -24,28 +24,28 @@ blockwise({'data': ('height', 'width')},
           name='__array_fmean2d')(__array_mean2d)
 
 @blockwise({'data': ('height', 'width')},
-           {'data': FloatArray, 'result': IntArray, ('height', 'width'): int, 'value': float32})
+           {'data': FloatArray, 'result': Int32Array, ('height', 'width'): int, 'value': float32})
 def __array_fgreater2d(data, result, value, height, width):
     for row in range(height):
         for col in range(width):
             result[row, col] = data[row, col] > value
 
 @blockwise({'data': ('height', 'width')},
-           {'data': FloatArray, 'result': IntArray, ('height', 'width'): int, 'value': float32})
+           {'data': FloatArray, 'result': Int32Array, ('height', 'width'): int, 'value': float32})
 def __array_fless2d(data, result, value, height, width):
     for row in range(height):
         for col in range(width):
             result[row, col] = data[row, col] < value
 
 @blockwise({'data': ('height', 'width')},
-           {('data', 'result'): FloatArray, 'values': IntArray, ('height', 'width'): int})
+           {('data', 'result'): FloatArray, 'values': Int32Array, ('height', 'width'): int})
 def __array_fimul2d(data, result, values, height, width):
     for row in range(height):
         for col in range(width):
             result[row, col] = data[row, col] * values[row, col]
 
 @blockwise({'data': ('height', 'width')},
-           {'data': IntArray, ('height', 'width'): int})
+           {'data': Int32Array, ('height', 'width'): int})
 def __array_invert2d(data, height, width):
     for row in range(height):
         for col in range(width):
@@ -62,7 +62,7 @@ _reductions = {
 }
 
 for name, func in _reductions.items():
-    for dtype in (IntArray, FloatArray):
+    for dtype in (Int32Array, FloatArray):
         name = '__%s_%s' % (dtype.__name__, name)
         globals()[name] = blockwise(
             {'x': (1,)}, {'x': dtype}, name=name, reduce=func)(array_do_nothing)
